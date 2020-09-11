bb9b9af4db83dcc40eb29833de63e13717a37f9b
Champloo <podilkaroman@gmail.com>
2020-09-11 11:41:19 +0300

    Changed the login/signup and don`t use admin

--- calories_book_project/urls.py
+++ calories_book_project/urls.py
@@ -1,5 +1,4 @@
 """calories_book_project URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -18,7 +17,6 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
--- users/admin.py
+++ users/admin.py
@@ -4,13 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username',]
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
 
 # Register your models here.
