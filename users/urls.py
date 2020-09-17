1cea639f2b1d5e4cd5a53d6c202a405b2417e637
Champloo <podilkaroman@gmail.com>
2020-09-18 00:35:22 +0300

    Added profile page, added update profile function

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('profile/edit/', views.ProfileUpdate.as_view(), name='profile_edit'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
