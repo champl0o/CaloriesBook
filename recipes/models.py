01a562547a5a05c4c8716dfd3a050235db36328c
Champloo <podilkaroman@gmail.com>
2021-01-03 21:48:47 +0200

    added small fixes

--- recipes/models.py
+++ recipes/models.py
@@ -5,8 +5,28 @@ from django.urls import reverse
 
 from django.utils.timezone import now
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    carbohydrates = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientItem(models.Model):
+    ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    ingredients = models.ManyToManyField(IngredientItem)
     title = models.CharField(max_length=50, verbose_name='Назва рецепту')
     pub_date = models.DateTimeField(default=now, editable=False)
     description = models.TextField(verbose_name='Опис')
@@ -34,22 +54,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length = 30)
-    proteins = models.FloatField()
-    fats = models.FloatField()
-    carbohydrates = models.FloatField()
-
-    def __str__(self):
-        return self.name
-
-
-class IngredientItem(models.Model):
-    ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE)
-    recipes = models.ManyToManyField(Recipe)
-    quantity = models.IntegerField()
-
-    def __str__(self):
-        return self.name
